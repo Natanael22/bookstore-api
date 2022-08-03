c984d4dea0e7f4c3265caa3c3bc450666039b741
Natanael22 <natanaelcorreia1@gmail.com>
2022-08-03 14:47:01 -0300

    interfaces

---
+++ src/main/java/br/com/natanael/bookstore/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package br.com.natanael.bookstore.repositories;
+
+import br.com.natanael.bookstore.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, int> {
+}
