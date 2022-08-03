d4a3c711de77daa70698a93b91a8d9fc9a86c56b
Natanael22 <natanaelcorreia1@gmail.com>
2022-08-03 14:43:07 -0300

    classes de dominio

---
+++ src/main/java/br/com/natanael/bookstore/domain/Livro.java
@@ -0,0 +1,84 @@
+package br.com.natanael.bookstore.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Livro  implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String titulo;
+    private String nomeAutor;
+    private String texto;
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
+    public Livro(int id, String titulo, String nomeAutor, String texto, Categoria categoria) {
+        this.id = id;
+        this.titulo = titulo;
+        this.nomeAutor = nomeAutor;
+        this.texto = texto;
+        this.categoria = categoria;
+    }
+
+    public Livro() {
+        super();
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getNomeAutor() {
+        return nomeAutor;
+    }
+
+    public void setNomeAutor(String nomeAutor) {
+        this.nomeAutor = nomeAutor;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Livro)) return false;
+        Livro livro = (Livro) o;
+        return id == livro.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
