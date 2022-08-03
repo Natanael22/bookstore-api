802b9cac8f658f45b55858134158622f70ab3826
Natanael22 <natanaelcorreia1@gmail.com>
2022-08-03 12:57:04 -0300

    iniciando projeto spring boot + angular

---
+++ src/test/java/br/com/natanael/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.natanael.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
